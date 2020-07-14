25cef1b30676ee579b86f5f3328c4fbb0aad4e56
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-14 22:08:09 +0900

    done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,15 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        s: Chars,
     }
+    let mut ans: String = String::new();
+    for i in 0..s.len() {
+        if s[i] == 'B' {
+            ans.pop();
+        } else {
+            ans.push_str(&s[i].to_string());
+        }
+    }
+    println!("{}", ans);
 }
